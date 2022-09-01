5a86a596ed5e60f620db8126a597f6ba285e169e
AutoReplySender <107805930+AutoReplySender@users.noreply.github.com>
2022-09-01 18:34:54 +0800

    join triggered comments

--- AutoReplyBot/Consumer.cs
+++ AutoReplyBot/Consumer.cs
@@ -27,37 +27,44 @@ public class Consumer
                 if (await db.CheckProcessed(comment)) continue;
                 var actions = await _matcher.Match(content, userName);
                 var (bandNo, postNo, commentId, subCommentId) = comment;
-                foreach (var action in actions)
+                var reply = string.Join("\n\n", actions.Select(a => a.ReplyContent));
+                var emotion = actions.FirstOrDefault(a => a.EmotionType != null)?.EmotionType;
+                Console.WriteLine($"Now replying {reply} to {bandNo} {postNo} {userName} {content}");
+                if (emotion != null)
                 {
-                    Console.WriteLine($"Now replying {action.ReplyContent} to {bandNo} {postNo} {userName} {content}");
-                    if (action.EmotionType != null)
-                    {
-                        Console.WriteLine($"Adding Emotion {action.EmotionType} to {bandNo} {postNo} {userName} {content}");
-                    }
-                    switch (comment)
-                    {
-                        case (_, _, 0, 0):
-                            await _bandClient.CreateCommentAsync(bandNo, postNo, action.ReplyContent);
-                            if (action.EmotionType != null)
-                            {
-                                await _bandClient.SetEmotionAsync(bandNo, postNo, action.EmotionType);
-                            }
-                            break;
-                        case (_, _, _, 0):
-                            await _bandClient.CreateCommentAsync(bandNo, postNo, commentId, action.ReplyContent, userNo, userName);
-                            if (action.EmotionType != null)
-                            {
-                                await _bandClient.SetEmotionAsync(bandNo, postNo, commentId, action.EmotionType);
-                            }
-                            break;
-                        default:
-                            await _bandClient.CreateCommentAsync(bandNo, postNo, commentId, action.ReplyContent, userNo, userName);
-                            if (action.EmotionType != null)
-                            {
-                                await _bandClient.SetEmotionAsync(bandNo, postNo, commentId, subCommentId, action.EmotionType);
-                            }
-                            break;
-                    }
+                    Console.WriteLine(
+                        $"Adding Emotion {emotion} to {bandNo} {postNo} {userName} {content}");
+                }
+
+                switch (comment)
+                {
+                    case (_, _, 0, 0):
+                        await _bandClient.CreateCommentAsync(bandNo, postNo, reply);
+                        if (emotion != null)
+                        {
+                            await _bandClient.SetEmotionAsync(bandNo, postNo, reply);
+                        }
+
+                        break;
+                    case (_, _, _, 0):
+                        await _bandClient.CreateCommentAsync(bandNo, postNo, commentId, reply, userNo,
+                            userName);
+                        if (emotion != null)
+                        {
+                            await _bandClient.SetEmotionAsync(bandNo, postNo, commentId, emotion);
+                        }
+
+                        break;
+                    default:
+                        await _bandClient.CreateCommentAsync(bandNo, postNo, commentId, reply, userNo,
+                            userName);
+                        if (emotion != null)
+                        {
+                            await _bandClient.SetEmotionAsync(bandNo, postNo, commentId, subCommentId,
+                                emotion);
+                        }
+
+                        break;
                 }
 
                 db.Add(comment);
